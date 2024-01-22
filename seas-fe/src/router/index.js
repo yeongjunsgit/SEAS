5070a5822e16ced597ea25c1fdffada3d9637e3a
MuRing <11esh@naver.com>
2024-01-22 14:06:24 +0900

    REFACTOR: import 주소 변경

--- seas-fe/src/router/index.js
+++ seas-fe/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import MenuView from "@/views/MenuView.vue";
-import MyPageView from "../views/MyPageView.vue";
+import MyPageView from "@/views/MyPageView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
