66a3f4ec0afe3b4de83cdace57ce95e5e51d9ea1
kky <badacura@gmail.com>
2024-01-22 15:37:36 +0900

    FEAT: 헤더 수정

--- seas-fe/src/components/commons/HeaderComponent.vue
+++ seas-fe/src/components/commons/HeaderComponent.vue
@@ -3,50 +3,66 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const goHome = () => {
-    router.push({ path: "/" });
+  router.push({ path: "/" });
 };
 
 const goMyPage = () => {
-    router.push({ path: "/mypage" });
+  router.push({ path: "/mypage" });
 };
 </script>
 
 <template>
-    <div>
-        <v-toolbar class="transparent">
-            <v-container>
-                <v-row justify="end">
-                    <v-col cols="4">
-                        <v-toolbar-title
-                            class="home-title text-center"
-                            @click="goHome()"
-                            >SEAS</v-toolbar-title
-                        >
-                    </v-col>
-                    <v-col cols="4" class="align-self-center">
-                        <v-toolbar-items>
-                            <v-btn>카드</v-btn>
-                            <v-btn>퀴즈</v-btn>
-                            <v-btn>랭킹</v-btn>
-                            <v-divider></v-divider>
-                            <v-btn @click="goMyPage()">마이페이지</v-btn>
-                            <v-btn>로그아웃</v-btn>
-                        </v-toolbar-items>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-toolbar>
-    </div>
+  <div>
+    <v-toolbar class="transparent">
+      <v-container>
+        <v-row justify="end">
+          <v-col cols="4">
+            <v-toolbar-title
+              class="home-title text-center text-h4 font-weight-bold"
+              @click="goHome()"
+              >SEAS</v-toolbar-title
+            >
+          </v-col>
+          <v-col cols="4" class="align-self-center">
+            <v-toolbar-items>
+              <v-btn class="home-menus">카드</v-btn>
+              <v-btn class="home-menus">퀴즈</v-btn>
+              <v-btn class="home-menus">랭킹</v-btn>
+              <v-divider></v-divider>
+              <v-btn class="home-menus" @click="goMyPage()">마이페이지</v-btn>
+              <v-btn class="home-menus">로그아웃</v-btn>
+            </v-toolbar-items>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-toolbar>
+  </div>
 </template>
 
 <style scoped lang="scss">
+@font-face {
+  font-family: "Ahnjunggeun";
+  src: url("@/assets/fonts/Ahnjunggeun.ttf");
+}
+@font-face {
+  font-family: "caveat";
+  src: url("@/assets/fonts/caveat.ttf");
+}
 .home-title {
-    &:hover {
-        cursor: pointer;
-    }
+  font-family: "caveat";
+  color: white;
+  &:hover {
+    cursor: pointer;
+  }
+}
+.home-menus {
+  font-family: "Ahnjunggeun";
+  font-size: larger;
+  font-weight: bold;
+  color: white;
 }
 .transparent {
-    background: transparent !important;
-    box-shadow: none !important;
+  background: transparent !important;
+  box-shadow: none !important;
 }
 </style>
--- seas-fe/src/views/MyPageView.vue
+++ seas-fe/src/views/MyPageView.vue
@@ -1,16 +1,40 @@
-<script setup></script>
+<script setup>
+import MyPageComponent from "@/components/mypage/MyPageComponent.vue";
+</script>
 
 <template>
-  <div>
-    <v-card
-      class="mx-auto"
-      max-width="100%"
-      height="1000px"
-      image="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      theme="dark"
-    >
-    </v-card>
-  </div>
+  <v-container>
+    <div class="background"></div>
+    <div class="rotate">
+      <br />
+      <br />
+      <h1 class="text-white mx-10">My Page</h1>
+
+      <v-row>
+        <v-col cols="6">
+          <MyPageComponent />
+        </v-col>
+        <v-col cols="6">
+          <MyPageComponent />
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.background {
+  rotate: 90deg;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+  background-image: url("@/assets/images/mypage_background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.rotate {
+  rotate: -90deg;
+}
+</style>
