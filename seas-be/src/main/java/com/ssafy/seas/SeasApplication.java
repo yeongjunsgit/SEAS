64e95889ed6a767d805a1a44782d1970bc4bbb3f
Jungeun Lee <silver36c@gmail.com>
2024-01-22 16:54:02 +0900

    FEAT: initialize project

---
+++ seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.seas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasApplication.class, args);
+	}
+
+}
