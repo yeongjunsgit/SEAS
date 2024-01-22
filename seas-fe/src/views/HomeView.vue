6aac69b7103eec7076bf6276713e9b3d1759da5c
MuRing <11esh@naver.com>
2024-01-22 15:18:19 +0900

    FEAT: Home Page Complete

--- seas-fe/src/views/HomeView.vue
+++ seas-fe/src/views/HomeView.vue
@@ -8,48 +8,85 @@ const goMenu = () => {
 </script>
 
 <template>
-    <div class="home_container">
-        <div></div>
-        <!-- 홈 화면 우측 부분 -->
-        <div>
-            <div></div>
-            <button class="main__wave_button" @click="goMenu()">
+    <div class="home-container">
+        <video muted autoplay loop>
+            <source src="@/assets/videos/home_sub.mp4" type="video/mp4" />
+            <strong>Your browser does not support the video tag.</strong>
+        </video>
+        <!-- 홈 화면 좌측 부분 -->
+        <div class="main-content">
+            <div>
+                <h1>분당 CS 10개</h1>
+                <h1>카드를 통한 간단한 암기</h1>
+                <h1>퀴즈를 통한 복기</h1>
+                <h1>점수를 통한 랭킹 시스템</h1>
+            </div>
+            <button class="wave-button" @click="goMenu()">
                 <span class="wave"></span>
                 <p>Button</p>
             </button>
         </div>
+        <!-- 홈 화면 우측 부분 -->
+        <div></div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.home_container {
+* {
+    width: 100%;
+    height: 100%;
+}
+.home-container {
     display: flex;
     width: 100%;
     height: 100%;
     justify-content: space-around;
 }
-.main__wave_button {
+
+video {
+    object-fit: cover;
+    position: fixed;
+    // transform: scale(1.8, 1.8);
+    // right: 40%;
+    // top: -20%;
+    min-width: 100%;
+    min-height: 100%;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+}
+
+.main-content {
+    margin-top: 10%;
+    color: white;
+    text-align: center;
+
+    h1 {
+        margin-bottom: 5%;
+    }
+}
+.wave-button {
     display: inline-block;
     width: 100px;
-    height: 61px;
+    height: 60px;
     position: relative;
     font-size: 20px;
     cursor: pointer;
-    padding: 0.8em 1em;
-    border: 3px solid #333;
+    border: 3px solid white;
     border-radius: 10px;
     overflow: hidden;
     box-shadow: 0 1px 3px rgba(black, 0.2);
     background: transparent;
     transform: translate3d(0, 0, 0);
     z-index: 1;
+    text-align: center;
 
     .wave {
         opacity: 1;
         position: absolute;
         bottom: 3%;
         left: 50%;
-        background: rgb(0, 0, 0);
+        background: rgb(45, 171, 255);
         width: 300px;
         height: 300px;
         margin-left: -150px;
@@ -69,14 +106,15 @@ const goMenu = () => {
         animation: rotate 2000ms infinite linear;
     }
 
-    &:hover p {
+    p {
+        top: 20%;
         position: relative;
         z-index: 3;
         transition: 1s ease-out;
+    }
+
+    &:hover p {
         color: white;
-        &:not(:hover) {
-            transition: 1s ease-out;
-        }
     }
 }
 
