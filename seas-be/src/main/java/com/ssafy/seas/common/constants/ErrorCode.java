381ac5e6f7c73e980e4641b06162adffd79ad973
Jungeun Lee <silver36c@gmail.com>
2024-01-23 21:12:16 +0900

    FEAT: 카테고리 이름 검증

--- seas-be/src/main/java/com/ssafy/seas/common/constants/ErrorCode.java
+++ seas-be/src/main/java/com/ssafy/seas/common/constants/ErrorCode.java
@@ -15,8 +15,8 @@ public enum ErrorCode {
 	ALREADY_DELETED(BAD_REQUEST, "이미 삭제된 값입니다"),
 	BAD_PARAMETER(BAD_REQUEST, "요청 파라미터가 잘못되었습니다."),
 	BAD_PARAMETER_TYPE(BAD_REQUEST, "지원하지 않는 파라미터 형식입니다."),
-	// 커뮤니티
-
+	// 카테고리
+	BAD_CATEGORY_NAME(BAD_REQUEST, "키테고리 이름이 잘못되었습니다."),
 	/* 500 INTERNAL_SERVER_ERROR : 서버 오류 */
 	SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 내부 오류로 인해 응답을 제공할 수 없습니다.");
 
--- seas-be/src/main/java/com/ssafy/seas/flashcard/controller/FlashcardController.java
+++ seas-be/src/main/java/com/ssafy/seas/flashcard/controller/FlashcardController.java
@@ -1,11 +1,15 @@
 package com.ssafy.seas.flashcard.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.seas.category.dto.CategoryDto;
+import com.ssafy.seas.category.service.CategoryService;
+import com.ssafy.seas.common.constants.ErrorCode;
 import com.ssafy.seas.common.constants.SuccessCode;
 import com.ssafy.seas.common.dto.ApiResponse;
 import com.ssafy.seas.flashcard.dto.FlashcardDto;
@@ -17,11 +21,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FlashcardController {
 	private final FlashcardService flashcardService;
+	private final CategoryService categoryService;
 
 	@GetMapping("/flashcard")
 	public ApiResponse<List<FlashcardDto.Response>> getFlashcards(@RequestParam("category") String categoryName) {
-		// TODO: categoryName 검증
-		
+		List<CategoryDto.Response> categories = categoryService.getCategories();
+
+		// categoryName 검증: categoryName과 일치하는 카테고리가 존재하지 않는다면
+		if (categories.stream().noneMatch(category -> categoryName.equals(category.getName()))) {
+			throw new NoSuchElementException(ErrorCode.BAD_CATEGORY_NAME.getMessage());
+		}
 		return ApiResponse.success(SuccessCode.GET_SUCCESS,
 			flashcardService.getFlashcaradsByCategoryName(categoryName));
 	}
