95f4e08fd5fa7eea7a7f9bda0c3814295f8eec6b
Jungeun Lee <silver36c@gmail.com>
2024-01-23 09:27:44 +0900

    FEAT: DB 연동

--- seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
+++ seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
@@ -2,10 +2,11 @@ package com.ssafy.seas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/seas-config.yml")
 public class SeasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SeasApplication.class, args);
 	}
