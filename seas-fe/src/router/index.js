6e12eb3769e7c097171363019b0588bfb63472e2
MuRing <11esh@naver.com>
2024-01-23 17:21:52 +0900

    FEAT: 랭킹 페이지 연결

--- seas-fe/src/router/index.js
+++ seas-fe/src/router/index.js
@@ -4,44 +4,50 @@ import MenuView from "@/views/MenuView.vue";
 import AuthenicateView from "@/views/AuthenicateView.vue";
 import MyPageView from "@/views/MyPageView.vue";
 import FlashcardView from "@/views/FlashcardView.vue";
+import RankingView from "@/views/RankingView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/menu",
-      name: "menu",
-      component: MenuView,
-    },
-    {
-      path: "/auth",
-      name: "authenicate",
-      component: AuthenicateView,
-    },
-    {
-      path: "/mypage/",
-      name: "mypage",
-      component: MyPageView,
-    },
-    {
-      path: "/flashcard/",
-      name: "flashcard",
-      component: FlashcardView,
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView,
+        },
+        {
+            path: "/menu",
+            name: "menu",
+            component: MenuView,
+        },
+        {
+            path: "/auth",
+            name: "authenicate",
+            component: AuthenicateView,
+        },
+        {
+            path: "/mypage/",
+            name: "mypage",
+            component: MyPageView,
+        },
+        {
+            path: "/flashcard/",
+            name: "flashcard",
+            component: FlashcardView,
+        },
+        {
+            path: "/ranking",
+            name: "ranking",
+            component: RankingView,
+        },
+        // {
+        //   path: '/about',
+        //   name: 'about',
+        //   // route level code-splitting
+        //   // this generates a separate chunk (About.[hash].js) for this route
+        //   // which is lazy-loaded when the route is visited.
+        //   component: () => import('../views/AboutView.vue')
+        // }
+    ],
 });
 
 export default router;
