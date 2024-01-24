7e9098a634d5faa10e43c6b0ad84756853e1d479
김영준 <dudwns020202@naver.com>
2024-01-24 17:09:32 +0900

    FEAT: flashcard button add

--- seas-fe/src/components/commons/HeaderComponent.vue
+++ seas-fe/src/components/commons/HeaderComponent.vue
@@ -3,83 +3,75 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const goHome = () => {
-    router.push({ path: "/" });
+  router.push({ path: "/" });
 };
 
 const goLogin = () => {
-    router.push({ path: "/auth" });
+  router.push({ path: "/auth" });
 };
 
 const goMyPage = () => {
-    router.push({ path: "/mypage" });
+  router.push({ path: "/mypage" });
 };
 
 const goCard = () => {
-    router.push({ path: "/flashcard" });
+  router.push({ path: "/flashcard" });
 };
 
 const goRank = () => {
-    router.push({ path: "/ranking" });
+  router.push({ path: "/ranking" });
 };
 </script>
 
 <template>
-    <div class="header-container">
-        <v-toolbar class="transparent">
-            <v-container>
-                <v-row justify="end">
-                    <v-col cols="4">
-                        <v-toolbar-title
-                            class="home-title text-center text-h4 font-weight-bold temp-font"
-                            @click="goHome()"
-                            >SEAS</v-toolbar-title
-                        >
-                    </v-col>
-                    <v-col cols="4" class="align-self-center">
-                        <v-toolbar-items class="text-font">
-                            <v-btn class="home-menus" @click="goCard()"
-                                >카드</v-btn
-                            >
-                            <v-btn class="home-menus">퀴즈</v-btn>
-                            <v-btn class="home-menus" @click="goRank()"
-                                >랭킹</v-btn
-                            >
-                            <v-divider></v-divider>
-                            <v-btn class="home-menus" @click="goLogin()"
-                                >로그인</v-btn
-                            >
-                            <v-btn class="home-menus" @click="goMyPage()"
-                                >마이페이지</v-btn
-                            >
-                            <v-btn class="home-menus">로그아웃</v-btn>
-                        </v-toolbar-items>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-toolbar>
-    </div>
+  <div class="header-container">
+    <v-toolbar class="transparent">
+      <v-container>
+        <v-row justify="end">
+          <v-col cols="4">
+            <v-toolbar-title
+              class="home-title text-center text-h4 font-weight-bold title-font"
+              @click="goHome()"
+              >SEAS</v-toolbar-title
+            >
+          </v-col>
+          <v-col cols="4" class="align-self-center">
+            <v-toolbar-items class="text-font">
+              <v-btn class="home-menus" @click="goCard()">카드</v-btn>
+              <v-btn class="home-menus">퀴즈</v-btn>
+              <v-btn class="home-menus" @click="goRank()">랭킹</v-btn>
+              <v-divider></v-divider>
+              <v-btn class="home-menus" @click="goLogin()">로그인</v-btn>
+              <v-btn class="home-menus" @click="goMyPage()">마이페이지</v-btn>
+              <v-btn class="home-menus">로그아웃</v-btn>
+            </v-toolbar-items>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-toolbar>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @import url("@/assets/style/main.scss");
 
 .header-container {
-    min-height: 80px;
-    vertical-align: middle;
+  min-height: 80px;
+  vertical-align: middle;
 }
 .home-title {
-    color: white;
-    &:hover {
-        cursor: pointer;
-    }
+  color: white;
+  &:hover {
+    cursor: pointer;
+  }
 }
 .home-menus {
-    font-size: larger;
-    font-weight: bold;
-    color: white;
+  font-size: larger;
+  font-weight: bold;
+  color: white;
 }
 .transparent {
-    background: transparent !important;
-    box-shadow: none !important;
+  background: transparent !important;
+  box-shadow: none !important;
 }
 </style>
