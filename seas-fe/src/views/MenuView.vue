c28e08ccaa8266b88d6d67e9b3e878f7e82f5a57
MuRing <11esh@naver.com>
2024-01-24 17:51:20 +0900

    FEAT: menu background video update

--- seas-fe/src/views/MenuView.vue
+++ seas-fe/src/views/MenuView.vue
@@ -7,6 +7,10 @@ const paths = ["/flashcard", "/quiz", "/ranking"];
 
 <template>
     <div class="menu-container">
+        <video muted autoplay loop>
+            <source src="@/assets/videos/menu.mp4" type="video/mp4" />
+            <strong>Your browser does not support the video tag.</strong>
+        </video>
         <MenuComponent
             v-for="idx in titles.length"
             :key="idx"
@@ -20,13 +24,25 @@ const paths = ["/flashcard", "/quiz", "/ranking"];
 <style scoped lang="scss">
 @import url("@/assets/style/main.scss");
 .menu-container {
-    background-image: url("@/assets/images/menu_background.jpg");
-    background-size: cover;
+    // background-image: url("@/assets/images/menu_background.jpg");
+    // background-size: cover;
     width: 100%;
     height: 100%;
     position: absolute;
     display: flex;
     justify-content: space-evenly;
     z-index: 0;
+}
+
+video {
+    // opacity: 0.7;
+    object-fit: cover;
+    position: fixed;
+    min-width: 100%;
+    min-height: 100%;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    filter: brightness(60%);
 }
 </style>
