85b8be51e3f7f6e9616b9cf9b2e93f8057c7872e
Jungeun Lee <silver36c@gmail.com>
2024-01-24 18:10:17 +0900

    FIX: 생성시각 수정시각 자동설정

--- seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
+++ seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @PropertySource("classpath:/seas-config.yml")
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
 public class SeasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SeasApplication.class, args);
