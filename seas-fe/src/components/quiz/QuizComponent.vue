21e2e1d534b31656c25c6cd6203a697e2fedb810
MuRing <11esh@naver.com>
2024-02-01 15:21:14 +0900

    REFACTOR: quiz page style refactor

--- seas-fe/src/components/quiz/QuizComponent.vue
+++ seas-fe/src/components/quiz/QuizComponent.vue
@@ -157,89 +157,52 @@ const clearInput = () => {
 
 <style scoped lang="scss">
 @import "@/assets/style/main.scss";
+@import "@/assets/style/quiz.scss";
 
-.quiz-container {
-    position: absolute;
-    background-image: url($url-path + "images/map.jpg");
-    background-size: cover;
-    height: 100%;
+.question-container {
     width: 100%;
-    display: flex;
-    justify-content: space-evenly;
+    min-height: 200px;
+    padding-top: 5%;
+}
+
+.hint-container {
+    height: 50px;
+}
+
+.result-container {
+    padding-top: 3%;
+    height: 50px;
+    text-align: center;
+}
+
+input {
+    width: 100%;
+    height: 100%;
+    padding: 7% 10% 7% 10%;
+    border: 4px double $primary-color;
     font-size: larger;
 
-    .quiz-content {
-        width: 50%;
-        height: 80%;
-        margin: 11vh 0 0 10%;
-        background-image: url($url-path + "images/quiz_bg.png");
-        background-size: 100% 100%;
-        background-position: center;
-        padding: 5% 7% 7% 7%;
-
-        .question-container {
-            width: 100%;
-            min-height: 200px;
-            padding-top: 5%;
-        }
-
-        .hint-container {
-            height: 50px;
-        }
-
-        .result-container {
-            padding-top: 3%;
-            height: 50px;
-            text-align: center;
-        }
-
-        input {
-            width: 100%;
-            height: 100%;
-            padding: 7% 10% 7% 10%;
-            border: 4px double $primary-color;
-            font-size: larger;
-
-            &:focus {
-                outline: none;
-            }
-        }
+    &:focus {
+        outline: none;
     }
+}
 
-    .quiz-button-container {
-        width: 27%;
-        height: 80%;
-        margin: 11vh 10% 0 0;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        font-size: xxx-large;
+.quiz-number {
+    background-image: url($url-path + "images/button_background.png");
+    background-size: 100% 12vh;
+    background-position: center;
+    min-width: 12vw;
+    min-height: 12vh;
+    text-align: center;
 
-        .quiz-number {
-            background-image: url($url-path + "images/button_background.png");
-            background-size: 100% 12vh;
-            background-position: center;
-            min-width: 12vw;
-            min-height: 12vh;
-            text-align: center;
-
-            p {
-                margin: 0;
-                padding-top: 2vh;
-            }
-        }
-
-        button {
-            width: 12vw;
-            padding: 3vh 2vh 2vh 2vh;
-            font-size: xx-large;
-        }
+    p {
+        margin: 0;
+        padding-top: 2vh;
     }
-    .isClicked {
-        transition-duration: 0.5s;
-        background-color: $selected-color;
-        color: white;
-    }
+}
+.isClicked {
+    transition-duration: 0.5s;
+    background-color: $selected-color;
+    color: white;
 }
 </style>
--- seas-fe/src/components/quiz/ResultComponent.vue
+++ seas-fe/src/components/quiz/ResultComponent.vue
@@ -41,35 +41,5 @@ const goMypage = () => {
 <style scoped lang="scss">
 // url이 아닌 바로 이렇게 직접적으로 써야 변수를 끌어다 쓸 수 있다.
 @import "@/assets/style/main.scss";
-
-.quiz-content {
-    width: 50%;
-    height: 80%;
-    margin: 11vh 0 0 10%;
-    background-image: url($url-path + "images/quiz_bg.png");
-    background-size: 100% 100%;
-    background-position: center;
-    padding: 5% 7% 7% 7%;
-}
-.quiz-button-container {
-    width: 27%;
-    height: 80%;
-    margin: 11vh 10% 0 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-size: xxx-large;
-
-    p {
-        margin: 0;
-        padding-top: 2vh;
-    }
-
-    button {
-        width: 12vw;
-        padding: 3vh 2vh 2vh 2vh;
-        font-size: xx-large;
-    }
-}
+@import "@/assets/style/quiz.scss";
 </style>
