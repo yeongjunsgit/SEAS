42e8cba891becb3a30417515bd9e4d2c6f61011c
이정은 <silver36c@gmail.com>
2024-02-04 18:28:07 +0900

    FEAT: 성적 추이 그래프 데이터 가져오기
    from 절에서의 JPQL 및 queryDSL 사용 불가 이슈로 NativeQuery 사용

--- seas-be/src/main/java/com/ssafy/seas/mypage/dto/MyPageDto.java
+++ seas-be/src/main/java/com/ssafy/seas/mypage/dto/MyPageDto.java
@@ -40,4 +40,43 @@ public class MyPageDto {
 	public static class PerformanceGraph {
 
 	}
+
+	@Getter
+	public static class ScoreHistory {
+
+		private LocalDateTime createdAt;
+		private Integer score;
+		private Integer round;
+
+		public ScoreHistory(Timestamp createdAt, Integer score, Long round) {
+			this.createdAt = createdAt.toLocalDateTime();
+			this.score = score;
+			this.round = Math.toIntExact(round);
+		}
+
+		@Builder
+		public ScoreHistory(LocalDateTime createdAt, Integer score, Integer round) {
+			this.createdAt = createdAt;
+			this.score = score;
+			this.round = round;
+		}
+
+	}
+
+	@Getter
+	public static class ScoreHistoryDetail extends ScoreHistory {
+
+		private Integer id;
+		private Integer categoryId;
+		private Integer memberId;
+
+		public ScoreHistoryDetail(Integer id, Timestamp createdAt, Integer score, Integer categoryId, Integer memberId,
+			Long round) {
+			super(createdAt, score, round);
+			this.id = id;
+			this.categoryId = categoryId;
+			this.memberId = memberId;
+		}
+	}
+
 }
--- seas-be/src/main/java/com/ssafy/seas/mypage/repository/MyPageRepository.java
+++ seas-be/src/main/java/com/ssafy/seas/mypage/repository/MyPageRepository.java
@@ -1,27 +1,19 @@
 package com.ssafy.seas.mypage.repository;
 
 import static com.ssafy.seas.category.entity.QCategory.*;
-import static com.ssafy.seas.member.entity.QMember.*;
 import static com.ssafy.seas.quiz.entity.QQuiz.*;
-import static com.ssafy.seas.quiz.entity.QScoreHistory.*;
 import static com.ssafy.seas.quiz.entity.QSolvedQuiz.*;
-import static com.ssafy.seas.ranking.entity.QTier.*;
 
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.Tuple;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.seas.member.dto.MemberDto;
-import com.ssafy.seas.member.dto.QMemberDto_MyInfoResponse;
 import com.ssafy.seas.mypage.dto.MyPageDto;
 import com.ssafy.seas.mypage.dto.QMyPageDto_CorrectCount;
-import com.ssafy.seas.quiz.entity.QScoreHistory;
-import com.ssafy.seas.quiz.entity.QSolvedQuiz;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Repository
 public class MyPageRepository {
 	private final JPAQueryFactory queryFactory;
+	private final EntityManager entityManager;
 
 	public List<MyPageDto.CorrectCount> getQuizCorrectCountPerCategory(Integer memberId) {
 
@@ -50,26 +43,37 @@ public class MyPageRepository {
 
 	}
 
-	public List<?> getScoreHistory(Integer memberId) {
-		// Assuming you have a DTO class named ScoreHistoryDTO to hold the result
+	// 성적 추이 그래프
+	public List<MyPageDto.ScoreHistoryDetail> getScoreHistory(Integer memberId) {
+		String nativeQuery = "SELECT " +
+			"id, " +
+			"created_at AS CreatedAt, " +
+			"score, " +
+			"category_id AS categoryId, " +
+			"member_id AS memberId, " +
+			"round " +
+			"FROM ( " +
+			"   SELECT " +
+			"       id, " +
+			"       created_at, " +
+			"       score, " +
+			"       category_id, " +
+			"       member_id, " +
+			"       RANK() OVER (PARTITION BY category_id ORDER BY created_at DESC) as row_num, " +
+			"       RANK() OVER (PARTITION BY category_id ORDER BY created_at ASC) as round " +
+			"   FROM " +
+			"       score_history " +
+			"   WHERE " +
+			"       member_id = :memberId" +
+			") ranked " +
+			"WHERE " +
+			"    row_num <= 5 " +
+			"ORDER BY category_id, round DESC";
 
-		List<?> result = queryFactory
-			.select(scoreHistory.id,
-				scoreHistory.createdAt,
-				scoreHistory.updatedAt,
-				scoreHistory.score,
-				scoreHistory.category.id, // Use category.id instead of categoryId
-				scoreHistory.member.id,
-				Expressions.numberTemplate(Integer.class, "row_number() over (partition by {0} order by {1} desc)", scoreHistory.category.id, scoreHistory.createdAt).as("rowNum"),
-				Expressions.numberTemplate(Integer.class, "row_number() over (partition by {0} order by {1} asc)", scoreHistory.category.id, scoreHistory.createdAt).as("round"))
-			.from(scoreHistory)
-			// .where(scoreHistory.member.id.eq(memberId), Expressions.numberTemplate(Integer.class, "row_number() over (partition by {0} order by {1} desc)", scoreHistory.category.id, scoreHistory.createdAt).loe(5))
-			// .orderBy(scoreHistory.category.id.asc(), Expressions.numberTemplate(Integer.class, "row_number() over (partition by {0} order by {1} asc)", scoreHistory.category.id, scoreHistory.createdAt).desc())
-			.where(scoreHistory.member.id.eq(memberId))
-			.orderBy(scoreHistory.category.id.asc())
-			.fetch();
+		Query query = entityManager.createNativeQuery(nativeQuery, MyPageDto.ScoreHistoryDetail.class);
+		query.setParameter("memberId", memberId);
 
-		log.info(result.toString());
+		List<MyPageDto.ScoreHistoryDetail> result = query.getResultList();
 
 		return result;
 	}
