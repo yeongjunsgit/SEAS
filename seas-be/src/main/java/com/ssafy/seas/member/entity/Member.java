212a85f2e5c83a3d2a26af390ebbcbbf05a4214f
이정은 <silver36c@gmail.com>
2024-02-04 21:27:01 +0900

    FEAT: mebmer 엔티티와 favorite 엔티티 연결

--- seas-be/src/main/java/com/ssafy/seas/member/entity/Member.java
+++ seas-be/src/main/java/com/ssafy/seas/member/entity/Member.java
@@ -1,9 +1,15 @@
 package com.ssafy.seas.member.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ssafy.seas.common.entity.BaseEntity;
+import com.ssafy.seas.flashcard.entity.Favorite;
+import com.ssafy.seas.quiz.entity.Quiz;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +31,6 @@ public class Member extends BaseEntity {
 	private String email;
 	private Integer point = 0;
 
+	@OneToMany(mappedBy = "member")
+	private List<Favorite> favorites = new ArrayList<>();
 }
