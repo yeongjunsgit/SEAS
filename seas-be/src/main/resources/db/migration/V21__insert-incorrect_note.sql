4dac39deb3a72267ea93a72ea67d3fd2c57302dc
이정은 <silver36c@gmail.com>
2024-02-06 22:10:11 +0900

    CHORE: 오답노트 더미데이터 추가

---
+++ seas-be/src/main/resources/db/migration/V21__insert-incorrect_note.sql
@@ -0,0 +1,3 @@
+insert into seas.incorrect_note (id, created_at, updated_at, member_id, quiz_id)
+values (1, '2024-02-06 21:45:51.000000', '2024-02-06 21:45:52.000000', 1, 3),
+       (2, '2024-02-06 21:46:29.000000', '2024-02-06 21:46:28.000000', 1, 1);
