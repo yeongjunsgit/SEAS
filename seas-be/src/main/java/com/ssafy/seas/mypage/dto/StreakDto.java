3f0da9a8d8266589ee6718754cddfc40589732dd
Jungeun Lee <silver36c@gmail.com>
2024-02-07 12:56:09 +0900

    FIX: Streak dto 시간 타입 변경

--- seas-be/src/main/java/com/ssafy/seas/mypage/dto/StreakDto.java
+++ seas-be/src/main/java/com/ssafy/seas/mypage/dto/StreakDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import com.querydsl.core.annotations.QueryProjection;
 
+import lombok.Builder;
 import lombok.Getter;
 
 public class StreakDto {
@@ -14,8 +15,9 @@ public class StreakDto {
 		private Integer grade;
 		private LocalDateTime createdAt;
 
-		public Response(Timestamp createdAt, Integer grade) {
-			this.createdAt = createdAt.toLocalDateTime();
+		@Builder
+		public Response(LocalDateTime createdAt, Integer grade) {
+			this.createdAt = createdAt;
 			this.grade = grade;
 		}
 	}
