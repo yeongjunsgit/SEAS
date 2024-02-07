eba9424c92e5348c287d5e6266d9151a711b0d80
Jungeun Lee <silver36c@gmail.com>
2024-02-07 10:19:55 +0900

    FEAT: 타임존 설정 (서울)

--- seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
+++ seas-be/src/main/java/com/ssafy/seas/SeasApplication.java
@@ -1,5 +1,7 @@
 package com.ssafy.seas;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
 
 @PropertySource("classpath:/seas-config.yml")
 @OpenAPIDefinition(servers = {@Server(url = "https://i10a609.p.ssafy.io/api", description="원격 백엔드 서버입니다.")})
@@ -16,6 +19,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @EnableJpaAuditing
 @EnableCaching
 public class SeasApplication {
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SeasApplication.class, args);
 	}
