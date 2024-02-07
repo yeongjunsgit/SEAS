dc4daea49021c2269ffa0a9d7474662883b676c1
송정훈 <sjhjack@naver.com>
2024-02-07 12:33:42 +0900

    FEAT: 시큐리티 초기 설정

--- seas-be/src/main/java/com/ssafy/seas/config/SecurityConfig.java
+++ seas-be/src/main/java/com/ssafy/seas/config/SecurityConfig.java
@@ -1,42 +1,58 @@
 package com.ssafy.seas.config;
 
-import static org.springframework.security.config.Customizer.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+	private final AuthenticationConfiguration authenticationConfiguration;
+
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.cors(CorsConfigurer::disable) // WebMvcConfig에 있는 cors 설정 사용
-			.csrf(CsrfConfigurer::disable);
+			// .csrf(CsrfConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable) //csrf 비활성
+			.httpBasic(AbstractHttpConfigurer::disable) //HTTP 기본인증 비활성
+			// 시큐리티가 세션을 만들지도 사용하지도 않음.
+			.sessionManagement((sessionManagement) ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+		;
 		return http.build();
+	}
+
+	@Bean
+	public AuthenticationFilter getAuthenticationFilter() throws Exception{
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+		authenticationFilter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+
+		return authenticationFilter;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
