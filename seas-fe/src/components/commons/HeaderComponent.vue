fefdb2402ddae11b00448aa2469cca6a882443f4
엄세현 <11esh@naver.com>
2024-02-11 16:03:58 +0900

    REFACTOR: Header content realligned

--- seas-fe/src/components/commons/HeaderComponent.vue
+++ seas-fe/src/components/commons/HeaderComponent.vue
@@ -12,94 +12,91 @@ const router = useRouter();
 const isLogin = ref(false);
 
 const movePage = (destination) => {
-    // 로그인 임시 코드
-    if (destination == "/auth") {
-        isLogin.value = true;
-    } else {
-        router.replace({ path: destination });
-    }
+  // 로그인 임시 코드
+  if (destination == "/auth") {
+    isLogin.value = true;
+  } else {
+    router.replace({ path: destination });
+  }
 };
 
 const logout = () => {
-    // 임시 코드
-    isLogin.value = false;
-    console.log("Logged out");
+  // 임시 코드
+  isLogin.value = false;
+  console.log("Logged out");
 };
 </script>
 
 <template>
-    <div class="header-container transparent">
-        <div class="header-content">
-            <div class="home-menus"></div>
-            <div class="home-title title-font" @click="movePage('/')">SEAS</div>
-            <div class="home-menus header-font">
-                <div class="menu">
-                    <button @click="movePage('/flashcard')">카드</button>
-                    <button @click="movePage('/quiz')">
-                        <p>퀴즈</p>
-                    </button>
-                    <button @click="movePage('/ranking')">랭킹</button>
-                </div>
-
-                <div class="menu">
-                    <button v-if="!isLogin" @click="movePage('/auth')">
-                        로그인 / 회원가입
-                    </button>
-
-                    <div v-else>
-                        <button @click="movePage('/mypage')">마이페이지</button>
-                        <button @click="logout">로그아웃</button>
-                    </div>
-                </div>
-            </div>
+  <div class="header-container">
+    <div class="header-content">
+      <div class="home-menus"></div>
+      <div class="home-title title-font" @click="movePage('/')">SEAS</div>
+      <div class="home-menus header-font">
+        <div class="menu">
+          <button @click="movePage('/flashcard')">카드</button>
+          <button @click="movePage('/quiz')">
+            <p>퀴즈</p>
+          </button>
+          <button @click="movePage('/ranking')">랭킹</button>
         </div>
+
+        <div class="menu">
+          <button v-if="!isLogin" @click="movePage('/auth')">로그인 / 회원가입</button>
+
+          <div v-else>
+            <button @click="movePage('/mypage')">마이페이지</button>
+            <button @click="logout">로그아웃</button>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @import "@/assets/style/main.scss";
 
 .header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 80px;
+  background: transparent !important;
+  box-shadow: none !important;
+  padding-top: 1%;
+
+  .header-content {
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: center;
-    min-height: 80px;
+    width: 100%;
 
-    .header-content {
-        display: flex;
-        justify-content: space-evenly;
-        align-items: center;
-        width: 100%;
+    .home-title {
+      width: 33%;
+      color: white;
+      font-weight: bolder;
+      font-size: 3vw;
+      text-align: center;
 
-        .home-title {
-            width: 33%;
-            color: white;
-            font-weight: bolder;
-            font-size: xx-large;
-            text-align: center;
-
-            &:hover {
-                cursor: pointer;
-            }
-        }
-        .home-menus {
-            width: 33%;
-            display: flex;
-            flex-wrap: nowrap;
-            color: white;
-            gap: 1vw;
-
-            font-size: 1.3vw;
-            font-weight: bold;
-
-            button {
-                padding: 0 10px 0 10px;
-            }
-        }
+      &:hover {
+        cursor: pointer;
+      }
     }
-}
-.transparent {
-    background: transparent !important;
-    box-shadow: none !important;
+    .home-menus {
+      width: 33%;
+      display: flex;
+      flex-wrap: nowrap;
+      color: white;
+      gap: 1vw;
+
+      font-size: 1.3vw;
+      font-weight: bold;
+
+      button {
+        padding: 0 10px 0 10px;
+      }
+    }
+  }
 }
 </style>
