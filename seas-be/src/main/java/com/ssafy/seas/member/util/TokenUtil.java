f1a0d516d8a57505b37c3df12529e9f4fd0a2470
송정훈 <sjhjack@naver.com>
2024-02-13 16:07:48 +0900

    REFACTOR: redis에 저장하는 key 타입 변경

--- seas-be/src/main/java/com/ssafy/seas/member/util/TokenUtil.java
+++ seas-be/src/main/java/com/ssafy/seas/member/util/TokenUtil.java
@@ -10,16 +10,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class TokenUtil {
-	private final RedisTemplate<Integer, String> redisTemplate;
+	private final RedisTemplate<String, String> redisTemplate;
 	private final MemberUtil memberUtil;
 
 	public void setRefreshToken(String refreshToken) {
-		redisTemplate.opsForValue().set(memberUtil.getLoginMemberId(), refreshToken);
+		redisTemplate.opsForValue().set("token" + memberUtil.getLoginMemberId(), refreshToken);
 	}
 
 	public String getRefreshToken() {
-		log.info("Redis에 저장되어 있는 토큰 ::::::::::::::: {}", redisTemplate.opsForValue().get(memberUtil.getLoginMemberId()));
-		return redisTemplate.opsForValue().get(memberUtil.getLoginMemberId());
+		log.info("Redis에 저장되어 있는 토큰 ::::::::::::::: {}", redisTemplate.opsForValue().get("token" + memberUtil.getLoginMemberId()));
+		return redisTemplate.opsForValue().get("token" + memberUtil.getLoginMemberId());
 	}
 
 	public boolean checkRefreshTokenEquals(String refreshToken) {
