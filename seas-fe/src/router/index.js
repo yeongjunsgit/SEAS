bbbac72aa1af81cf22a67f5a844c428c746973b6
kky <badacura@gmail.com>
2024-02-14 09:49:18 +0900

    FEAT: 라우터가드 설정

--- seas-fe/src/router/index.js
+++ seas-fe/src/router/index.js
@@ -9,78 +9,80 @@ import QuizView from "@/views/QuizView.vue";
 import PopupCardView from "@/views/PopupCardView.vue";
 import PopupQuizView from "@/views/PopupQuizView.vue";
 import PreMyPageView from "@//views/PreMyPageView.vue";
+import { useauthControllerStore } from "@/stores/authController.js";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/menu",
+      name: "menu",
+      component: MenuView,
+    },
+    {
+      path: "/auth",
+      name: "authenicate",
+      component: AuthenicateView,
+    },
+    {
+      path: "/mypage",
+      name: "premypage",
+      component: PreMyPageView,
+      children: [
         {
-            path: "/",
-            name: "home",
-            component: HomeView,
+          path: "",
+          name: "mypage",
+          component: MyPageView,
         },
         {
-            path: "/menu",
-            name: "menu",
-            component: MenuView,
+          path: "popupcard",
+          name: "popupcard",
+          component: PopupCardView,
         },
         {
-            path: "/auth",
-            name: "authenicate",
-            component: AuthenicateView,
+          path: "popupquiz",
+          name: "popupquiz",
+          component: PopupQuizView,
         },
-        {
-            path: "/mypage",
-            name: "premypage",
-            component: PreMyPageView,
-            children: [
-                {
-                    path: "",
-                    name: "mypage",
-                    component: MyPageView,
-                },
-                {
-                    path: "popupcard",
-                    name: "popupcard",
-                    component: PopupCardView,
-                },
-                {
-                    path: "popupquiz",
-                    name: "popupquiz",
-                    component: PopupQuizView,
-                },
-            ],
-        },
-        {
-            path: "/flashcard/",
-            name: "flashcard",
-            component: FlashcardView,
-        },
-        {
-            path: "/ranking",
-            name: "ranking",
-            component: RankingView,
-        },
-        {
-            path: "/quiz",
-            name: "quiz",
-            component: QuizView,
-        },
-        // 예외 처리 페이지
-        {
-            path: "/:pathMatch(.*)*",
-            component: () => import("@/views/ErrorView.vue"),
-        },
-    ],
+      ],
+    },
+    {
+      path: "/flashcard/",
+      name: "flashcard",
+      component: FlashcardView,
+    },
+    {
+      path: "/ranking",
+      name: "ranking",
+      component: RankingView,
+    },
+    {
+      path: "/quiz",
+      name: "quiz",
+      component: QuizView,
+    },
+    // 예외 처리 페이지
+    {
+      path: "/:pathMatch(.*)*",
+      component: () => import("@/views/ErrorView.vue"),
+    },
+  ],
 });
 
 router.beforeEach((to, from, next) => {
-    const isAuthenticated = asdf;
+  const userStore = useauthControllerStore();
+  const isAuthenticated = userStore.isAuthenticated;
 
-    if (to.name !== "auth" && !isAuthenticated) {
-        next({ name: "auth" }); // 로그인 페이지로 리다이렉트
-    } else {
-        next(); // 계속 진행
-    }
+  if (to.name !== "home" && to.name !== "authenicate" && !isAuthenticated) {
+    next({ name: "authenicate" }); // 로그인 페이지로 리다이렉트
+  } else {
+    next(); // 계속 진행
+  }
 });
 
 export default router;
