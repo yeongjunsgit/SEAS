a6a0480921e99ba3f09186232e7aee7d7113dfb8
MuRing <11esh@naver.com>
2024-02-14 09:36:13 +0900

    FEAT: router guard set

--- seas-fe/src/router/index.js
+++ seas-fe/src/router/index.js
@@ -11,74 +11,76 @@ import PopupQuizView from "@/views/PopupQuizView.vue";
 import PreMyPageView from "@//views/PreMyPageView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/menu",
-      name: "menu",
-      component: MenuView,
-    },
-    {
-      path: "/auth",
-      name: "authenicate",
-      component: AuthenicateView,
-    },
-    {
-      path: "/mypage",
-      name: "premypage",
-      component: PreMyPageView,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: "",
-          name: "mypage",
-          component: MyPageView,
+            path: "/",
+            name: "home",
+            component: HomeView,
         },
         {
-          path: "popupcard",
-          name: "popupcard",
-          component: PopupCardView,
+            path: "/menu",
+            name: "menu",
+            component: MenuView,
         },
         {
-          path: "popupquiz",
-          name: "popupquiz",
-          component: PopupQuizView,
+            path: "/auth",
+            name: "authenicate",
+            component: AuthenicateView,
         },
-      ],
-    },
-    {
-      path: "/flashcard/",
-      name: "flashcard",
-      component: FlashcardView,
-    },
-    {
-      path: "/ranking",
-      name: "ranking",
-      component: RankingView,
-    },
-    {
-      path: "/quiz",
-      name: "quiz",
-      component: QuizView,
-    },
-    // 예외 처리 페이지
-    {
-      path: "/:pathMatch(.*)*",
-      component: () => import("@/views/ErrorView.vue"),
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ],
+        {
+            path: "/mypage",
+            name: "premypage",
+            component: PreMyPageView,
+            children: [
+                {
+                    path: "",
+                    name: "mypage",
+                    component: MyPageView,
+                },
+                {
+                    path: "popupcard",
+                    name: "popupcard",
+                    component: PopupCardView,
+                },
+                {
+                    path: "popupquiz",
+                    name: "popupquiz",
+                    component: PopupQuizView,
+                },
+            ],
+        },
+        {
+            path: "/flashcard/",
+            name: "flashcard",
+            component: FlashcardView,
+        },
+        {
+            path: "/ranking",
+            name: "ranking",
+            component: RankingView,
+        },
+        {
+            path: "/quiz",
+            name: "quiz",
+            component: QuizView,
+        },
+        // 예외 처리 페이지
+        {
+            path: "/:pathMatch(.*)*",
+            component: () => import("@/views/ErrorView.vue"),
+        },
+    ],
+});
+
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = asdf;
+
+    if (to.name !== "auth" && !isAuthenticated) {
+        next({ name: "auth" }); // 로그인 페이지로 리다이렉트
+    } else {
+        next(); // 계속 진행
+    }
 });
 
 export default router;
